d3de2984975284ca1f3b4ad78fdaf5970b4f0bd7
Jernej Rozman <jernejrozman@outlook.com>
2025-01-03 19:18:52 +0100

    updated player class

--- Vap_hero/game/Player.js
+++ Vap_hero/game/Player.js
@@ -1,8 +1,31 @@
-/*  TODO:
+// Player.js
+import { FirstPersonController } from 'engine/controllers/FirstPersonController.js';
 
-    Player mora imeti:
-        => Physics engine
-        => Transformacije za premik <= FirstPersonController
-        => Vap  TODO make in blender
+export class Player {
+    constructor(camera, canvas) {
+        // Povežemo kamero, platno in FPC
+        this.camera = camera;
+        this.canvas = canvas;
 
-*/
+        // Na kamero dodamo FirstPersonController
+        this.controller = new FirstPersonController(camera, canvas);
+        this.camera.addComponent(this.controller);
+
+        // Definiramo AABB (Axis-Aligned Bounding Box) za igralca
+        this.camera.isDynamic = true;
+        this.camera.aabb = {
+            min: [-0.2, -0.2, -0.2],
+            max: [0.2, 0.2, 0.2],
+        };
+    }
+
+    update(time, dt) {
+        // Če bi želeli neko dodatno logiko za igralca v vsakem frame-u,
+        // jo vpišemo sem.
+        // Tvoj FirstPersonController že vsebuje funkcijo update v svoji koda,
+        // zato jo engine (oz. node) običajno sam kliče.
+
+        // Primer: recimo, da bi morali ročno klicati FPC:
+        // this.controller.update?.(time, dt);
+    }
+}
